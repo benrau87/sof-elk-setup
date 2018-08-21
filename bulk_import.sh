3bfff38ed096cf57b391cb65f28f9b22d2bc84f6
benrau87 <benrau87@users.noreply.github.com>
2018-08-21 14:52:12 -0500

    Update bulk_import.sh

--- bulk_import.sh
+++ bulk_import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+shopt -s nullglob
 for evtlog in $(find /logstash/hostlogs/ -name  "*.evtx"); do
   python /home/elk_user/evtxtoelk/evtxtoelk.py $evtlog 127.0.0.1:9200
   echo "Adding $evtlog to ES"
